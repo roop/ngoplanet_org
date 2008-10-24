83e10760841c86bd2fd7aacaffcd8cb242e0adda
Roopesh Chander <roop@forwardbias.in>
2008-10-25 01:36:15 +0530

    Now that we have feeditemtemplate, we can write our own opml dumper too

--- cronjobs/runrawdog.sh
+++ cronjobs/runrawdog.sh
@@ -8,5 +8,7 @@ echo "summary"
 ./rawdog-2.11/rawdog -d rawdog-config/ -c summary/config --write --verbose
 echo "rss"
 ./rawdog-2.11/rawdog -d rawdog-config/ -c rss/config --write --verbose
+echo "opml"
+./rawdog-2.11/rawdog -d rawdog-config/ -c opml/config --write --verbose
 echo "DONE"
 date
