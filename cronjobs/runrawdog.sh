373ac47295f5fa0761005f20f9d0602db250a188
Roopesh Chander <roop@forwardbias.in>
2008-10-25 01:44:30 +0530

    explcitly invoke python to ensure we get the correct python version in the host machine

--- cronjobs/runrawdog.sh
+++ cronjobs/runrawdog.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 date > ngoplanet.log
 echo "frontpage" >> ngoplanet.log
-./rawdog-2.11/rawdog -d rawdog-config/ -c config --update --write --verbose 2>> ngoplanet.log
+/usr/local/bin/python ./rawdog-2.11/rawdog -d rawdog-config/ -c config --update --write --verbose 2>> ngoplanet.log
 echo "bloglist" >> ngoplanet.log
-./rawdog-2.11/rawdog -d rawdog-config/ -c bloglist/config --write --verbose 2>> ngoplanet.log
+/usr/local/bin/python ./rawdog-2.11/rawdog -d rawdog-config/ -c bloglist/config --write --verbose 2>> ngoplanet.log
 echo "summary" >> ngoplanet.log
-./rawdog-2.11/rawdog -d rawdog-config/ -c summary/config --write --verbose 2>> ngoplanet.log
+/usr/local/bin/python ./rawdog-2.11/rawdog -d rawdog-config/ -c summary/config --write --verbose 2>> ngoplanet.log
 echo "rss" >> ngoplanet.log
-./rawdog-2.11/rawdog -d rawdog-config/ -c rss/config --write --verbose 2>> ngoplanet.log
+/usr/local/bin/python ./rawdog-2.11/rawdog -d rawdog-config/ -c rss/config --write --verbose 2>> ngoplanet.log
 echo "opml" >> ngoplanet.log
-./rawdog-2.11/rawdog -d rawdog-config/ -c opml/config --write --verbose 2>> ngoplanet.log
+/usr/local/bin/python ./rawdog-2.11/rawdog -d rawdog-config/ -c opml/config --write --verbose 2>> ngoplanet.log
 echo "DONE" >> ngoplanet.log
 date >> ngoplanet.log
