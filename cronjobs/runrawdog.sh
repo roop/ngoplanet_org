3b19a3645e037a6228489518d1ed4f9b343551c4
Roopesh Chander <roop@forwardbias.in>
2008-10-25 01:42:28 +0530

    Redirect output to a log file

--- cronjobs/runrawdog.sh
+++ cronjobs/runrawdog.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-date
-echo "frontpage"
-./rawdog-2.11/rawdog -d rawdog-config/ -c config --update --write --verbose
-echo "bloglist"
-./rawdog-2.11/rawdog -d rawdog-config/ -c bloglist/config --write --verbose
-echo "summary"
-./rawdog-2.11/rawdog -d rawdog-config/ -c summary/config --write --verbose
-echo "rss"
-./rawdog-2.11/rawdog -d rawdog-config/ -c rss/config --write --verbose
-echo "opml"
-./rawdog-2.11/rawdog -d rawdog-config/ -c opml/config --write --verbose
-echo "DONE"
-date
+date > ngoplanet.log
+echo "frontpage" >> ngoplanet.log
+./rawdog-2.11/rawdog -d rawdog-config/ -c config --update --write --verbose 2>> ngoplanet.log
+echo "bloglist" >> ngoplanet.log
+./rawdog-2.11/rawdog -d rawdog-config/ -c bloglist/config --write --verbose 2>> ngoplanet.log
+echo "summary" >> ngoplanet.log
+./rawdog-2.11/rawdog -d rawdog-config/ -c summary/config --write --verbose 2>> ngoplanet.log
+echo "rss" >> ngoplanet.log
+./rawdog-2.11/rawdog -d rawdog-config/ -c rss/config --write --verbose 2>> ngoplanet.log
+echo "opml" >> ngoplanet.log
+./rawdog-2.11/rawdog -d rawdog-config/ -c opml/config --write --verbose 2>> ngoplanet.log
+echo "DONE" >> ngoplanet.log
+date >> ngoplanet.log
